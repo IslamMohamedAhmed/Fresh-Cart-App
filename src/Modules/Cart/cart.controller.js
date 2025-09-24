a48465cc08b8965762084b0719aadfa207979ad2
IslamMohamedAhmed <islammohamedahmed49@gmail.com>
2025-09-24 05:54:01 +0300

    initializing cart module

--- src/Modules/Cart/cart.controller.js
+++ src/Modules/Cart/cart.controller.js
@@ -1,4 +1,5 @@
 import { cartModel } from "../../../Database/Models/cart.model.js";
+import { couponModel } from "../../../Database/Models/coupon.model.js";
 import { productModel } from "../../../Database/Models/product.model.js";
 import { catchError } from "../../Middlewares/catchError.js";
 import { appError } from "../../Utils/appError.js";
@@ -13,10 +14,17 @@ const addToCart = catchError(async (req, res, next) => {
             next(new appError('invalid quatity!!', 401));
         }
         else {
+            req.body.price = product.price;
             let cart = new cartModel({
                 user: tokenUser.id,
                 cartItems: [req.body]
             });
+            cart.totalPrice = cart.cartItems.reduce((acc, item) => {
+                return acc + (item.quantity * item.price);
+            }, 0);
+            if (cart.discount) {
+                cart.totalPriceAfterDiscount = cart.totalPrice * (1 - cart.discount / 100);
+            }
             await cart.save();
             res.json({ message: 'success', cart });
         }
@@ -34,19 +42,120 @@ const addToCart = catchError(async (req, res, next) => {
         }
         else {
             if (req.body.quantity > product.quantity) {
-                next(new appError('invalid quatity!!', 401));
+                next(new appError('invalid quantity!!', 401));
             }
             else {
+                req.body.price = product.price;
                 result.cartItems.push(req.body);
             }
+        }
+        result.totalPrice = result.cartItems.reduce((acc, item) => {
+            return acc + (item.quantity * item.price);
+        }, 0);
+        if (result.discount) {
+            result.totalPriceAfterDiscount = result.totalPrice * (1 - result.discount / 100);
         }
         await result.save();
         res.json({ message: 'success', result });
     }
 });
 
+const removeFromCart = catchError(async (req, res, next) => {
+    let tokenUser = req.headers['user-info'];
+    let cart = await cartModel.findOne({ user: tokenUser.id });
+    let productExist = cart.cartItems.find(item => item._id.equals(req.params.id));
+    if (productExist) {
+        let result = await cartModel.findOneAndUpdate({ user: tokenUser.id }, { $pull: { cartItems: { _id: req.params.id } } }, { new: true });
+        result.totalPrice = result.cartItems.reduce((acc, item) => {
+            return acc + (item.quantity * item.price);
+        }, 0);
+        await result.save();
+        res.json({ message: 'success', result });
+    }
+    else {
+        next(new appError('product is not found in your cart!!', 404));
+    }
+});
+
+const updateQuantity = catchError(async (req, res, next) => {
+    let tokenUser = req.headers['user-info'];
+    let cart = await cartModel.findOne({ user: tokenUser.id });
+    let productExist = cart.cartItems.find(item => item.product.equals(req.params.id));
+    if (productExist) {
+        let product = await productModel.findById(productExist.product);
+        if (req.body.quantity > product.quantity) {
+            return next(new appError('invalid quantity!!', 401));
+        }
+        else {
+            productExist.quantity = req.body.quantity;
+            cart.totalPrice = cart.cartItems.reduce((acc, item) => {
+                return acc + (item.quantity * item.price);
+            }, 0);
+            if (cart.discount) {
+                cart.totalPriceAfterDiscount = cart.totalPrice * (1 - cart.discount / 100);
+            }
+            await cart.save();
+            res.json({ message: 'success', cart });
+
+        }
+    }
+    else {
+        next(new appError('product is not found in your cart!!', 404));
+    }
+
+});
+
+const getLoggedUserCart = catchError(async (req, res, next) => {
+    let tokenUser = req.headers['user-info'];
+    let cart = await cartModel.findOne({ user: tokenUser.id }).populate('cartItems.product');
+    if (cart) {
+        res.json({ message: 'success', cart });
+    }
+    else {
+        next(new appError('your cart is empty!!', 401));
+    }
+});
+
+const clearUserCart = catchError(async (req, res, next) => {
+    let tokenUser = req.headers['user-info'];
+    let cart = await cartModel.findOne({ user: tokenUser.id });
+    if (cart) {
+        let result = await cartModel.findOneAndDelete({ user: tokenUser.id });
+        res.json({ message: 'success', result });
+    }
+    else {
+        next(new appError('your cart is already empty!!', 401));
+    }
+});
+
+const applyCoupon = catchError(async (req, res, next) => {
+    let tokenUser = req.headers['user-info'];
+    let coupon = await couponModel.findOne({ code: req.body.coupon, expiresAt: { $gte: Date.now() } });
+    if (coupon) {
+        let cart = await cartModel.findOne({ user: tokenUser.id });
+        if (cart) {
+            cart.discount = coupon.discount;
+            cart.totalPriceAfterDiscount = cart.totalPrice * (1 - cart.discount / 100);
+            await cart.save();
+            res.json({ message: 'success', cart });
+        }
+        else {
+            next(new appError('cart is not found!!', 404));
+        }
+    }
+    else {
+        next(new appError('coupon is not found!!', 404));
+    }
+});
+
+
 
 
 export {
-    addToCart
+    addToCart,
+    removeFromCart,
+    updateQuantity,
+    getLoggedUserCart,
+    clearUserCart,
+    applyCoupon
 }
--- src/Modules/Cart/cart.routes.js
+++ src/Modules/Cart/cart.routes.js
@@ -2,9 +2,9 @@ import express from "express";
 import { validateToken } from "../../Middlewares/validateToken.js";
 import { findById } from "../../Middlewares/findById.js";
 import { validateInputs } from "../../Middlewares/validateInputs.js";
-import { validateAddToCart } from "./cart.validation.js";
+import { validateAddToCart, validateParamsId, validateUpdateQuantity } from "./cart.validation.js";
 import { productModel } from "../../../Database/Models/product.model.js";
-import { addToCart } from "./cart.controller.js";
+import { addToCart, applyCoupon, clearUserCart, getLoggedUserCart, removeFromCart, updateQuantity } from "./cart.controller.js";
 
 
 const cartRouter = express.Router();
@@ -12,6 +12,8 @@ const cartRouter = express.Router();
 cartRouter.use(validateToken);
 
 cartRouter.route('/').patch(validateInputs(validateAddToCart),
-    findById({ model: productModel, foreignKey: 'product', from: 'body', necessary: true, objectName: 'product' }), addToCart);
-
+    findById({ model: productModel, foreignKey: 'product', from: 'body', necessary: true, objectName: 'product' }), addToCart)
+    .get(getLoggedUserCart).delete(clearUserCart);
+cartRouter.route('/:id').delete(validateInputs(validateParamsId), removeFromCart).put(validateInputs(validateUpdateQuantity), updateQuantity);
+cartRouter.route('/applyCoupon').patch(applyCoupon);
 export default cartRouter;
